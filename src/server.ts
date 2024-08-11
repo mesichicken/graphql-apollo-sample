fc9c3f95c135fd4720aa025899b20b11188f15fe
mesi <mesicharida202@gmail.com>
2024-08-11 19:23:54 +0900

    sqliteの導入

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,23 @@
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
 import fs from 'fs'
+import { PrismaClient } from '@prisma/client'
 
-const links = [
-  {
-    id: 'link-0',
-    description: 'Fullstack tutorial for GraphQL',
-    url: 'www.howtographql.com',
-  },
-]
+const prisma = new PrismaClient()
 
 const resolvers = {
   Query: {
     info: () => 'HackerNews Clone',
-    feed: () => links,
+    feed: (parent, args, context) => context.prisma.link.findMany(),
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${links.length}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link)
+    post: async (parent, args, context) => {
+      const link = await context.prisma.link.create({
+        data: {
+          description: args.description,
+          url: args.url,
+        },
+      })
       return link
     }
   }
@@ -30,11 +25,14 @@ const resolvers = {
 
 const server = new ApolloServer({
   typeDefs: fs.readFileSync('src/schema.graphql', 'utf8'),
-  resolvers
+  resolvers,
 })
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
+  context: async () => ({
+    prisma,
+  }),
 })
 
-console.log(`🚀 Server ready at ${url}`)
+console.log(`Server is running on ${url}`)
